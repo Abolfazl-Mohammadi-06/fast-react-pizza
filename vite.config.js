7a5361ad01d776aaf94bf49a79b6daedf2451837
AbolfazlMohammadi <abolfazl.mohammadi.85.2006@gmail.com>
2026-08-30 17:05:55 +0330

    Deploy React app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // base: "/fast-react-pizza/",
+    plugins: [react()],
+    base: process.env.NODE_ENV === "production"
+        ? "/fast-react-pizza/"
+        : "/",
 })
